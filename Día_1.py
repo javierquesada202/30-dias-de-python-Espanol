55a9ff793c156b620fae2d84a6470df1e4cd5bab
javierquesada202 <118718647+javierquesada202@users.noreply.github.com>
2023-06-20 22:15:13 +0200

    Add files via upload

---
+++ Día_1.py
@@ -0,0 +1,27 @@
+#hola a todos
+#este es el mismo proyecto de Asabeneh, si quereis visitarlo y apoyarlo ayudariais mucho
+
+#primero aprenderemos los tipos de datos en python
+print(type(10))              #Esto es un nuumero entero, lo encontraremos como Int
+print(type(3.14))            #Esto es numero decimal, lo encontraremos como Float
+print(type(3+2j))            #Esto es un numero complejo, lo encontraremos como Complex
+print(type('Javierquesada')) #Esto es una palabra, la encontraremos como String
+
+#ahora vamos a aprender los tipos de lista
+print(type[1, 2, 3, 4])                #esto es una lista, lo encontraremos como List
+print(type({'name'; 'Javierquesada'})) #esto es un diccionario, lo encontraremos como Dictionary
+print(type({9.8, 3.14, 2.7}))          #esto es un set, lo encontraremos como Set
+print(type((9.8, 3.14, 2.7)))          #esto es un tuple, lo encontraremos como Tuple
+
+#ahora aprenderemos las operaciones basicas, que son las siguientes:
+print(2 + 3)   # suma(+)
+print(3 - 1)   # resta(-)
+print(2 * 3)   # multiplicación(*)
+print(3 / 2)   # división(/)
+print(3 ** 2)  # potencia(**)
+print(3 % 2)   # resto de la division(%)
+print(3 // 2)  # division entera (Int) (//)
+
+#Esto ha sido la leccion de hoy.
+#gracias por leer, si quieren darle a la estrellita, me ayudarian mucho
+#Hasta mañana!
